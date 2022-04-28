ae43165f3e05785d8796b69ac74f215b112ebc9a
inheritance10 <ali552001a@gmail.com>
2022-04-28 09:34:40 +0300

    .

--- routes/web.php
+++ routes/web.php
@@ -1,79 +1,88 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DefaultController;
 use App\Http\Controllers\SettingsController;
 use App\Http\Controllers\BlogController;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\SliderController;
 use App\Http\Controllers\UserController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 
-
 Route::prefix('admin')->group(function () {
+
     Route::get('/',[DefaultController::class,'index'])
         ->name('admin.index');
 
+    Route::get('/login',[DefaultController::class,'login'])
+        ->name('admin.login');
 
-    Route::get('/settings',[SettingsController::class,'index'])
+
+});
+
+
+Route::prefix('admin/settings')->group(function () {
+
+    Route::get('/', [SettingsController::class, 'index'])
         ->name('settings.index');
-
-    Route::post('/sortable',[SettingsController::class,'sortable'])
+    Route::post('/sortable', [SettingsController::class, 'sortable'])
         ->name('settings.Sortable');
-
-    Route::get('/settings/delete/{id}',[SettingsController::class,'Destroy']);
-
-    Route::get('/settings/edit/{id}',[SettingsController::class,'edit'])
+    Route::get('/settings/delete/{id}', [SettingsController::class, 'Destroy']);
+    Route::get('/settings/edit/{id}', [SettingsController::class, 'edit'])
         ->name('settings.edit');
-
-    Route::post('/update/{id}',[SettingsController::class,'update'])
+    Route::post('/update/{id}', [SettingsController::class, 'update'])
         ->name('settings.update');
 });
 
 
-
-Route::prefix('admin')->group(function (){
+Route::prefix('admin')->group(function () {
     //blog modülü
-    Route::resource('blog',BlogController::class);
+    Route::resource('blog', BlogController::class);
 
-    Route::post('blog/sortable',[BlogController::class,'sortable'])
+    Route::post('blog/sortable', [BlogController::class, 'sortable'])
         ->name('blog.Sortable');
 
 
     //Page modülü
 
-    Route::resource('page',PageController::class);
+    Route::resource('page', PageController::class);
 
-    Route::post('page/sortable',[PageController::class,'sortable'])
+    Route::post('page/sortable', [PageController::class, 'sortable'])
         ->name('page.Sortable');
 
     //Slider modülü
 
-    Route::resource('slider',SliderController::class);
+    Route::resource('slider', SliderController::class);
 
-    Route::post('slider/sortable',[SliderController::class,'sortable'])
+    Route::post('slider/sortable', [SliderController::class, 'sortable'])
         ->name('slider.Sortable');
 
     //Admin modülü
 
-    Route::resource('user',UserController::class);
+    Route::resource('user', UserController::class);
 
-    Route::post('user/sortable',[UserController::class,'sortable'])
+    Route::post('user/sortable', [UserController::class, 'sortable'])
         ->name('user.Sortable');
 });
+
 
 
 
